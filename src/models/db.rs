2df39567fa3a42e087ec408db154a3aac6a53aea
klover-rs <joelkerbusch@gmail.com>
2024-10-19 11:28:03 +0200

    first impl of Database filters

--- src/models/db.rs
+++ src/models/db.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 // the actions we perform for the database
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum DatabaseAction {
     Insert,
     Delete,
@@ -13,32 +13,39 @@ pub enum DatabaseAction {
 }
 
 // the ordering direction
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
 pub enum OrderDirection {
     Asc,
     Desc,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct OrderBy {
     pub column: String,
     pub direction: OrderDirection,
 }
 
 // the filters aka the search conditions
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Filters {
-    pub where_clause: Option<HashMap<String, Value>>,
+    #[serde(rename = "where")]
+    pub where_clause: Option<HashMap<String, Value>>, // use in the request actually "where" instead of "where_clause"
     pub order_by: Option<OrderBy>,
     pub limit: Option<u32>,
     pub offset: Option<u32>,
 }
 
 // the request struct itself
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct DatabaseRequest {
     pub table: String,
     pub action: DatabaseAction,
     pub values: Option<HashMap<String, Value>>,
     pub filters: Option<Filters>,
+}
+
+pub struct QueryBuilder {
+    pub table: String,
+    pub filters: Option<Filters>,
+    pub bind_params: Vec<Value>
 }
--- src/validation/db.rs
+++ src/validation/db.rs
@@ -1,7 +1,9 @@
 use crate::{
-    models::db::{DatabaseAction, DatabaseRequest, Filters, OrderDirection},
+    models::db::{DatabaseAction, DatabaseRequest, Filters, OrderDirection, QueryBuilder},
     to_string_,
 };
+use anyhow::Result;
+use serde_json::Value;
 use std::str::FromStr;
 
 impl FromStr for DatabaseAction {
@@ -41,7 +43,19 @@ impl Default for Filters {
 }
 
 impl DatabaseRequest {
-    pub fn validate(&self) -> Result<(), String> {
+    pub fn validate(&mut self) -> Result<(), String> {
+
+        // eleminate spaces
+        self.table = self.table.split_whitespace().collect::<String>();
+
+        // check if all characters are either a-z 0-9 or contain a '_' 
+        if !self.table.chars().all(|c| c.is_ascii_alphanumeric() || c == '_') {
+            return Err(to_string_!("Table name contains invalid characters. Only alphanumeric characters and underscores are allowed."));
+        }
+
+        // conver to lowercase chars
+        self.table = self.table.to_ascii_lowercase();
+
         match self.action {
             DatabaseAction::Insert => {
                 if self.values.is_none() || self.values.as_ref().unwrap().is_empty() {
@@ -51,11 +65,82 @@ impl DatabaseRequest {
             DatabaseAction::Delete => {}
             DatabaseAction::Update => {
                 if self.values.is_none() || self.values.as_ref().unwrap().is_empty() {
-                    return Err("Update action requires non-empty values.".to_string());
+                    return Err(to_string_!("Update action requires non-empty values."));
                 }
             }
             DatabaseAction::Retrieve => {}
         }
         Ok(())
+    }
+}
+
+impl QueryBuilder {
+    pub fn new(table: String, filters: Option<Filters>) -> Self {
+        Self {
+            table,
+            filters,
+            bind_params: Vec::new(),
+        }
+    }
+
+    pub fn build_query(&mut self) -> Result<(String, Vec<Value>)> {
+        /* 
+        ==========================================
+        = DANGER: POTENTIAL SQL INJECTION RISK! =
+        ==========================================
+        */
+        // if you are directly embedding table names in queries, 
+        // this can make your code VULNERABLE TO SQL INJECTION ATTEMPTS.
+        // if you use the DatabaseRequest::validate() function 
+        // and provide the table argument only if the validate function passed, 
+        // you should have nothing to worry about.
+        let mut query = format!("SELECT * FROM {}", self.table);
+
+        let mut bind_index = 1;
+
+        if let Some(filters) = &self.filters {
+            println!("Filters: {:?}", filters);
+            let (where_clause_sql, bind_values) = filters.build_where_caluse(&mut bind_index)?;
+            query.push_str(&where_clause_sql);
+            self.bind_params.extend(bind_values);
+
+        }
+
+        Ok((query, self.bind_params.clone()))
+    }
+
+}
+
+impl Filters {
+    fn sanitize_column_name(column: &str) -> Result<String> {
+        let is_valid = column.chars().all(|c| c.is_ascii_alphanumeric() || c == '_');
+        if !is_valid {
+            return Err(anyhow::anyhow!("Invalid column name: {}", column));
+        }
+        Ok(column.to_string())
+    }
+
+    pub fn build_where_caluse(&self, bind_index: &mut usize) -> Result<(String, Vec<Value>)>{
+        let mut conditions = Vec::new();
+        let mut bind_values = Vec::new();
+
+        if let Some(where_clause) = &self.where_clause {
+            for (column, value) in where_clause {
+                let sanitized_column = Self::sanitize_column_name(column)?;
+
+                let condition = format!("{} = ${}", sanitized_column, *bind_index);
+                conditions.push(condition);
+
+                bind_values.push(value.clone());  
+                *bind_index += 1; 
+            }
+        }
+
+        if !conditions.is_empty() {
+            Ok((format!(" WHERE {}", conditions.join(" AND ")), bind_values))
+        } else {
+            Ok((String::new(), Vec::new()))  // if no conditions, return an empty WHERE clause
+        }
+
     }
 }
